de4d0926008c24063fd3ad1ceb19fb82722834ac
신종윤 <tigerjong123@email.com>
2026-07-15 11:34:25 +0900

    feat: cors 추가

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
 from datetime import datetime
 
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from app.database import Base, engine
 from app.models.post import Post
-from app.routers.post_router import router as post_router
 from app.routers.map_router import router as map_router
+from app.routers.post_router import router as post_router
 
 
-# 서버 실행 시 등록된 SQLAlchemy 모델의 테이블 생성
+# 등록된 SQLAlchemy 모델을 기반으로 테이블 생성
 Base.metadata.create_all(bind=engine)
 
 
@@ -20,7 +21,20 @@ app = FastAPI(
 )
 
 
-# 예외 처리기
+# 프론트엔드 서버의 API 요청 허용
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# HTTP 예외 처리
 @app.exception_handler(StarletteHTTPException)
 async def http_exception_handler(
     request: Request,
@@ -39,12 +53,14 @@ async def http_exception_handler(
     )
 
 
-# 헬스 체크 용 api
+# 헬스 체크 API
 @app.get("/")
 def health_check() -> dict[str, str]:
     return {
         "message": "LocalHub 서버가 정상적으로 실행 중입니다."
     }
 
+
+# 라우터 등록
 app.include_router(post_router)
 app.include_router(map_router)
